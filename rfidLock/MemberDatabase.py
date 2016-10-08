4362647030459632eb638db055b9c870296c8ce1
Matthew Balch <matbalch@vt.edu>
2016-10-08 14:06:55 -0400

    Members adding and checking are tested and work

--- rfidLock/MemberDatabase.py
+++ rfidLock/MemberDatabase.py
@@ -2,6 +2,7 @@
 # There are certain
 
 import hashlib
+from contextlib import closing
 
 # Used to abstract database details a little bit more
 class MemberDatabase(object):
@@ -14,17 +15,19 @@ class MemberDatabase(object):
   The RFID data is hashed before use to prevent abuse
   """
   def __init__(self, db, subs):
-    self.cur = db.cursor()
+    """
+    db - database object to use
+    subs - substitution expression for the database in use
+    """
+    self.db = db
     self.start_query = """
       CREATE TABLE member_table (
-        id INT NOT NULL AUTO_INCREMENT,
+        hash BLOB(32),
         name TEXT,
         email TEXT,
-        hash BLOB(32),
         revoked BOOLEAN DEFAULT FALSE,
-        CONSTRAINT pk_id PRIMARY KEY(id),
-        CONSTRAINT unique_email UNIQUE(email),
-        CONSTRAINT unique_hash UNIQUE(hash));
+        CONSTRAINT pk_hash PRIMARY KEY(hash),
+        CONSTRAINT unique_email UNIQUE(email));
       """
     self.destroy_query = """
       DROP TABLE member_table;
@@ -36,70 +39,72 @@ class MemberDatabase(object):
       UPDATE member_table SET revoked=TRUE WHERE email={0};
       """.format(subs)
     self.revoked_query = """
-      SELECT id FROM member_table WHERE revoked=TRUE;
+      SELECT hash FROM member_table WHERE revoked=TRUE;
       """
     self.reinstate_query = """
       UPDATE member_table SET revoked=FALSE WHERE email={0};
       """.format(subs)
     self.have_query = """
-      SELECT id FROM member_table WHERE hash={0};
+      SELECT COUNT(hash) FROM member_table WHERE hash={0};
       """.format(subs)
     self.have_current_query = """
-      SELECT id FROM member_table WHERE hash={0} AND revoked=FALSE;
+      SELECT COUNT(hash) FROM member_table WHERE hash={0} AND revoked=FALSE;
       """.format(subs)
     self.list_query = """
       SELECT name, email, revoked FROM member_table;
       """
     self.content_query = """
-      SELECT id, name, email, hash, revoked FROM member_table;
+      SELECT hash, name, email, revoked FROM member_table;
       """
     self.clone_query = """
-      INSERT INTO member_table (id, name, email, hash, revoked) VALUES {0};
+      INSERT INTO member_table (hash, name, email, revoked) VALUES {0};
       """.format(subs)
-  def __del__(self):
-    self.cur.close()
   def hash(self, card_data):
     """Hashes the provided RFID data using MD5"""
     m = hashlib.md5()
-    m.update(card_data.encode())
+    m.update(card_data)
     return m.digest()
   def add(self, card_data, member_name, member_email):
     """Adds a new member to the list of members"""
-    self.cur.execute(self.add_query, (member_name, member_email, self.hash(card_data)))
+    with closing(self.db.cursor()) as cur:
+      cur.execute(self.add_query, (member_name, member_email, self.hash(card_data)))
   def revoke(self, email):
     """Marks a member as no longer a current member of the space."""
-    self.cur.execute(self.revoke_query, (email, ))
-    return cur.rowcount > 0
-  def revoked(self):
-    """Retrieves the table ids of the revoked members"""
-    self.cur.execute(self.revoked_query)
-    return self.cur.fetchall()
+    with closing(self.db.cursor()) as cur:
+      cur.execute(self.revoke_query, (email, ))
+      return cur.rowcount > 0
   def reinstate(self, email):
     """Marks a former member as a current member of the space again."""
-    self.cur.execute(self.reinstate_query, (email, ))
+    with closing(self.db.cursor()) as cur:
+      cur.execute(self.reinstate_query, (email, ))
   def have(self, card_data):
     """
     Uses the hash of the member's RFID data to check whether they have ever
     been a member.
     """
-    self.cur.execute(self.have_query, (self.hash(card_data), ))
-    return cur.rowcount > 0
+    with closing(self.db.cursor()) as cur:
+      cur.execute(self.have_query, (self.hash(card_data), ))
+      return cur.fetchone()[0] > 0
   def have_current(self, card_data):
     """
     Uses the member's RFID data to check whether they are a current member.
     """
-    self.cur.execute(self.have_current_query, (self.hash(card_data), ))
-    return cur.rowcount > 0
+    with closing(self.db.cursor()) as cur:
+      cur.execute(self.have_current_query, (self.hash(card_data), ))
+      return cur.fetchone()[0] > 0
   def list(self):
     """Retrieves a list of all members and former members"""
-    self.cur.execute(self.list_query)
-    return cur.fetchall()
-  def start(self):
+    with closing(self.db.cursor()) as cur:
+      cur.execute(self.list_query)
+      return cur.fetchall()
+  def create(self):
     """Creates the tables necessary for the membership system"""
-    self.cur.execute(self.start_query)
+    with closing(self.db.cursor()) as cur:
+      cur.execute(self.start_query)
   def destroy(self):
     """Removes the tables created for this system"""
-    self.cur.execute(self.destroy_query)
+    with closing(self.db.cursor()) as cur:
+      cur.execute(self.destroy_query)
   def clear(self):
     """Resets the contents of this database to be empty"""
     self.destroy()
@@ -107,6 +112,7 @@ class MemberDatabase(object):
   def mimic(self, other):
     """Makes this database identical to the provided database"""
     self.clear()
-    other.cur.execute(other.content_query)
-    self.cur.execute(self.clone_query, other.cur.fetchall())
+    with closing(self.db.cursor()) as cur, closing(other.db.cursor()) as othercur:
+      othercur.execute(other.content_query)
+      cur.execute(self.clone_query, other.cur.fetchall())
 
--- rfidLock/test/TestMemberDatabase.py
+++ rfidLock/test/TestMemberDatabase.py
@@ -4,21 +4,50 @@ import unittest
 import sqlite3
 from context import rfidLock
 from rfidLock import MemberDatabase
+from os import remove
 import random, string
 
 class TestMemberDatabaseHash(unittest.TestCase):
-  def test_not_nop(self):
+  def test_hash_mutates(self):
     # Check that the hash isn't a nop 
-    self.assertNotEqual(MemberDatabase.hash(None, "hello"), "hello")
-  def test_few_collide(self):
+    self.assertNotEqual(MemberDatabase.hash(None, "hello".encode()), "hello".encode())
+  def test_hash_does_not_collide(self):
     # Check that hash doesn't collide with 2000 unique random strings
-    self.assertEqual(len(frozenset([MemberDatabase.hash(None, ''.join(random.sample(string.ascii_lowercase + string.ascii_uppercase + string.digits, 10))) for i in range(0, 2000)])), 2000)
+    self.assertEqual(len(frozenset([MemberDatabase.hash(None, ''.join(random.sample(string.ascii_lowercase + string.ascii_uppercase + string.digits, 10)).encode()) for i in range(0, 2000)])), 2000)
 
-class TestMemberDatabaseStart(unittest.TestCase):
-  pass
+class TestMemberDatabaseCreate(unittest.TestCase):
+  db_path = "/tmp/test_member_database_create.db"
+  def setUp(self):
+    self.db = sqlite3.connect(self.db_path)
+    self.member_db = MemberDatabase(self.db, "?")
+  def tearDown(self):
+    # close the connection and delete the object
+    self.db.close()
+    remove(self.db_path)
+  def test_can_create(self):
+    # Check that database creation doesn't cause an error
+    self.member_db.create()
+  def test_double_creation_fails(self):
+    # Check that trying to create an existing database throws an error
+    self.member_db.create()
+    with self.assertRaises(self.db.OperationalError):
+      self.member_db.create()
 
 class TestMemberDatabaseDestroy(unittest.TestCase):
-  pass
+  db_path = "/tmp/test_member_database_destroy.db"
+  def setUp(self):
+    self.db = sqlite3.connect(self.db_path)
+    self.member_db = MemberDatabase(self.db, "?")
+  def tearDown(self):
+    # close the connection and delete the object
+    self.db.close()
+    remove(self.db_path)
+  def test_creation_destruction_cycles_work(self):
+    # Check that database creation doesn't cause an error
+    self.member_db.create()
+    self.member_db.destroy()
+    self.member_db.create()
+    self.member_db.destroy()
 
 class TestMemberDatabaseClear(unittest.TestCase):
   pass
@@ -27,7 +56,33 @@ class TestMemberDatabaseMimic(unittest.TestCase):
   pass
 
 class TestMemberDatabaseAdd(unittest.TestCase):
-  pass
+  db_path = "/tmp/test_member_database_add.db"
+  def setUp(self):
+    self.db = sqlite3.connect(self.db_path)
+    self.member_db = MemberDatabase(self.db, "?")
+    self.member_db.create()
+  def tearDown(self):
+    # close the connection and delete the object
+    self.db.close()
+    remove(self.db_path)
+  def test_add_member_does_not_fail(self):
+    self.member_db.add(b'test_data', "John Smith", "js@hackrva.org")
+
+class TestMemberDatabaseHave(unittest.TestCase):
+  db_path = "/tmp/test_member_database_add.db"
+  def setUp(self):
+    self.db = sqlite3.connect(self.db_path)
+    self.member_db = MemberDatabase(self.db, "?")
+    self.member_db.create()
+    self.member_db.add(b'test_data', "John Smith", "js@hackrva.org")
+  def tearDown(self):
+    # close the connection and delete the object
+    self.db.close()
+    remove(self.db_path)
+  def test_checks_member_existence(self):
+    self.assertTrue(self.member_db.have(b'test_data'))
+  def test_checks_member_non_existence(self):
+    self.assertFalse(self.member_db.have(b'bad_test_data'))
 
 class TestMemberDatabaseRevoke(unittest.TestCase):
   pass
@@ -36,9 +91,6 @@ class TestMemberDatabaseRevoked(unittest.TestCase):
   pass
 
 class TestMemberDatabaseReinstate(unittest.TestCase):
-  pass
-
-class TestMemberDatabaseHave(unittest.TestCase):
   pass
 
 class TestMemberDatabaseHaveCurrent(unittest.TestCase):
