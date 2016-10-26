73702c2db36f604804f46177181a6c916e3c0d1e
Matthew Balch <matbalch@vt.edu>
2016-10-26 02:36:13 -0400

    Moved template files to be explicitly part of rfidLock module

--- setup.py
+++ setup.py
@@ -8,15 +8,19 @@ setup(
   version='0.1',
   packages = ['rfidLock', 'rfidDoor'],
   scripts = [
+    'bin/rfid_config',
     'bin/rfid_db_install',
     'bin/rfid_db_remove',
     'bin/rfid_db.wsgi',
     'bin/rfid_door'],
   install_requires = [
+    # "Must not install sqlite3 from pypi"
     'mysql-connector',
     'RPIO',
-    'sqlite3',
-    'jinja2'])
+    'jinja2'],
+  package_data = {
+    'rfidLock': ['templates/*.json', 'templates/*.html'],
+  })
 
 # TODO Create /etc/rfidlock/config.json
 
